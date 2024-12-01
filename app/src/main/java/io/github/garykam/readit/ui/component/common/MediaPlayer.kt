c201f1c0998e40f325303669ae015ecb1e046fe8
Gary Kam <garyhkam@gmail.com>
2024-11-30 21:07:32 -0800

    Volume toggle in video player

--- app/src/main/java/io/github/garykam/readit/ui/component/common/MediaPlayer.kt
+++ app/src/main/java/io/github/garykam/readit/ui/component/common/MediaPlayer.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.AndroidEmbeddedExternalSurface
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,10 +24,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import io.github.garykam.readit.R
 
 @OptIn(UnstableApi::class)
 @Composable
@@ -66,7 +73,7 @@ fun MediaPlayer(
         AnimatedVisibility(visible = !isPlaying) {
             Icon(
                 imageVector = Icons.Default.PlayArrow,
-                contentDescription = "play",
+                contentDescription = "play media",
                 modifier = Modifier
                     .scale(2F)
                     .background(
@@ -74,6 +81,28 @@ fun MediaPlayer(
                         shape = CircleShape
                     )
             )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(5.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            var isMuted by remember { mutableStateOf(false) }
+            IconButton(
+                onClick = {
+                    isMuted = !isMuted
+                    exoPlayer.volume = if (isMuted) 0F else 1F
+                }
+            ) {
+                Icon(
+                    painter = painterResource(if (isMuted) R.drawable.ic_volume_mute else R.drawable.ic_volume),
+                    contentDescription = "volume",
+                    modifier = Modifier.size(20.dp)
+
+                )
+            }
         }
     }
 }
